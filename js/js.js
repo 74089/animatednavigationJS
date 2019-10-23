6669e17e7c20728898cb9f9f9a5f4bc939caf53e
Stef <25659722+74089@users.noreply.github.com>
2019-10-23 16:18:55 +0200

    Added the function
    Function makes a menu pop up and dissapear

--- js/js.js
+++ js/js.js
@@ -26,4 +26,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function menuButton(x) {
   x.classList.toggle("change");
+    var x = document.getElementById("wrapperStickyMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function showMenu() {
+  var x = document.getElementById("wrapperStickyMenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
